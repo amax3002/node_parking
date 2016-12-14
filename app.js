e206958b8e4f3de11851b5a4b8286ce0ac9ea20a
amax3002 <alexandremguy@gmil.com>
2016-12-14 15:57:07 -0500

    level 1 complete need to refactor

--- app.js
+++ app.js
@@ -5,14 +5,46 @@ var inputFile='parking_feb_2016.csv';
 
 
 var csvData=[];
+var parkingType=[];
 
 fs.createReadStream(inputFile)
-    .pipe(parse({delimiter: ','}))
+    .pipe(parse({delimiter: ','},{columns: true}))
     .on('data', function(csvrow) {
+
         // console.log(csvrow);
         csvData.push(csvrow);
     })
     .on('end',function() {
-      console.log((csvData.length) - 1);
+
+
+      csvData.forEach(function getViolationColumn(columnSelected) {
+        parkingType.push(columnSelected.VIOLATION_DESCRIPTION);
+});
+var count = {};
+parkingType.forEach(function seperationAndCounting(ticket) {
+  if(!count[ticket]) {
+    count[ticket] = 1;
+  } else {
+    count[ticket] ++;
+  }
+})
+
+var sortable = [];
+for (var ticketEach in count)
+    sortable.push([ticketEach, count[ticketEach]]);
+
+sortable.sort(function(a, b) {
+    return b[1] - a[1];
+})
+
+console.log(sortable);
+console.log(sortable[0]);
+console.log(sortable.length);
+
+//We need to be able to generate a new unique key for an array based on values in getViolationColumn
+
+// console.log(count);
+
+      // console.log(parkingType);
 
     });
--- parsing.js
+++ parsing.js
@@ -1,31 +1,18 @@
 var fs = require('fs');
 var parse = require('csv-parse');
-var async = require('async');
 
 var inputFile='parking_feb_2016.csv';
 
 
-// var nameList = inputFile
-//     .split(',')
-//     .map(function convertName(name) {
-//       return name.toUpperCase();
-//   })
-//   .filter(function(name) {
-//     return name.trim().length > 0;
-//   })
-//
-//   console.log(nameList);
-
-
 var csvData=[];
+
 fs.createReadStream(inputFile)
-    .pipe(parse({delimiter: ':'}))
+    .pipe(parse({delimiter: ','}))
     .on('data', function(csvrow) {
-        console.log(csvrow);
-        //do something with csvrow
+        // console.log(csvrow);
         csvData.push(csvrow);
     })
     .on('end',function() {
-      //do something wiht csvData
-      console.log(csvData);
+      console.log((csvData.length) - 1);
+
     });
